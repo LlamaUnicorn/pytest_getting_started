8514455c95afdcc1ae26bcfce96fe588e2bcaf35
LlamaUnicorn <holidaynight@gmail.com>
2022-12-09 15:58:23 +0300

    initial commit

---
+++ practice.py
@@ -0,0 +1,8 @@
+def my_sum(a, b):
+    return a + b
+
+
+# test 2
+
+m = my_sum(1, 2)
+print(m)
---
+++ test_practice.py
@@ -0,0 +1,6 @@
+from practice import my_sum
+
+
+def test_my_sum():
+    test_unit = my_sum(1, 3)
+    assert test_unit == 4
